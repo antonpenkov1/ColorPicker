6aefe7e1d4c68feb2ec99911161b607391479bb6
anton_penkov <anton.penkov1@gmail.com>
2023-11-17 15:39:22 +0100

    working app

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -8,26 +8,54 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
+// MARK: - Outlets
     @IBOutlet var colorView: UIView!
     
-    @IBOutlet var redComponent: UILabel!
-    @IBOutlet var greenComponent: UILabel!
-    @IBOutlet var blueComponent: UILabel!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
     
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    private var redNumber: CGFloat = 0.05
+    private var greenNumber: CGFloat = 0.27
+    private var blueNumber: CGFloat = 0.49
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorView.layer.cornerRadius = 16
+        setViewColor()
     }
 
+    // MARK: - IBActions
     @IBAction func redSliderAction() {
+        redNumber = CGFloat(redSlider.value)
+        redLabel.text = String(format: "%.2f", redNumber)
+        setViewColor()
     }
     @IBAction func greenSliderAction() {
+        greenNumber = CGFloat(greenSlider.value)
+        greenLabel.text = String(format: "%.2f", greenNumber)
+        setViewColor()
     }
     @IBAction func blueSliderAction() {
+        blueNumber = CGFloat(blueSlider.value)
+        blueLabel.text = String(format: "%.2f", blueNumber)
+        setViewColor()
+    }
+    
+    // MARK: - Private Methods
+    private func setViewColor() {
+        colorView.backgroundColor = UIColor(
+            red: redNumber,
+            green: greenNumber,
+            blue: blueNumber,
+            alpha: 1
+        )
     }
     
 }
