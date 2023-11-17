beedbfa4b86799fac378668fcc462e86e04e1053
anton_penkov <anton.penkov1@gmail.com>
2023-11-17 14:47:19 +0100

    Setup of storyboard and adding IBOutlets and IBActions

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -9,10 +9,26 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redComponent: UILabel!
+    @IBOutlet var greenComponent: UILabel!
+    @IBOutlet var blueComponent: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func redSliderAction() {
+    }
+    @IBAction func greenSliderAction() {
+    }
+    @IBAction func blueSliderAction() {
+    }
+    
 }
 
